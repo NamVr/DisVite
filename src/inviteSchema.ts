726d9b89282afaea2ee22ca2e7af517c88ac09cc
Naman Vrati <info@namanvrati.cf>
2025-06-18 18:15:59 +0530

    🚀 | Invite Schema

---
+++ src/inviteSchema.ts
@@ -0,0 +1,84 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface InviteSchema extends Document {
+	// The ID of the guild this invite belongs to.
+	guildId: string;
+
+	// The ID of the person who was invited.
+	inviteeId: string;
+
+	// The user ID of the user who created the invite.
+	inviterId?: string | null;
+
+	// The ID of the invite, discord.gg/invite/{inviteCode}
+	inviteCode?: string | null;
+
+	// The type of the invite.
+	joinType: "normal" | "vanity" | "unknown";
+
+	// The timestamp when the user joined the guild.
+	joinedAt: Date;
+
+	// The timestamp when the user left the guild, if applicable.
+	leftAt?: Date | null;
+
+	// Whether the invite is fake or not (detected by module).
+	fake: boolean;
+}
+
+const InviteSchema: Schema = new Schema({
+	// The ID of the guild this invite belongs to.
+	guildId: {
+		type: String,
+		required: true,
+		index: true,
+	},
+
+	// The ID of the person who was invited.
+	inviteeId: {
+		type: String,
+		required: true,
+		index: true,
+	},
+
+	// The user ID of the user who created the invite.
+	inviterId: {
+		type: String,
+		index: true,
+		default: null, // null if the inviter is unknown.
+	},
+
+	// The ID of the invite, discord.gg/invite/{inviteCode}
+	inviteCode: {
+		type: String,
+		default: null, // null if the invite is unknown.
+	},
+
+	// The type of the invite.
+	// "normal" for regular invites, "vanity" for vanity URLs, "unknown" for unknown invites.
+	joinType: {
+		type: String,
+		enum: ["normal", "vanity", "unknown"],
+		required: true,
+	},
+
+	// The timestamp when the user joined the guild.
+	joinedAt: {
+		type: Date,
+		required: true,
+	},
+
+	// The timestamp when the user left the guild, if applicable.
+	leftAt: {
+		type: Date,
+		default: null, // only if applicable.
+	},
+
+	// Whether the invite is fake or not (detected by module).
+	fake: {
+		type: Boolean,
+		default: false,
+	},
+});
+
+export default mongoose.model<InviteSchema>("inviteSchema", InviteSchema);
